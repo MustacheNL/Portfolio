8710b92072d247a86a3319ff74d06aee5bf81d12
Nyma Dolatkhahnejad <mustachedeliva@gmail.com>
2017-09-22 09:47:46 +0200

    Added installation page, fixed some code + cleaned

---
+++ panel/installation/index.php
@@ -0,0 +1,42 @@
+<?php
+require '../../private/config.php';
+?>
+<html>
+    <head>
+        <title>
+            Installatie
+        </title>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    </head>
+
+    <style>
+        .m-t-10 {
+            margin-top: 10px;
+        }
+
+        .panel-primary {
+            margin: auto;
+            margin-top: 50%;
+        }
+    </style>
+
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-md-offset-3">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">Installatie: Welkom</div>
+                        <div class="panel-body">
+                            <h2>Hallo!</h2><br/>
+                            Bedankt voor het downloaden van het Portfolio systeem + ingebouwd admin paneel.
+                            Om gebruik te kunnen maken van het paneel zijn er wat gegevens benodigd van jou!
+                            Klik snel verder om te kijken hoe simpel, maar toch geweldig dit systeem is.
+                            <br/><br/>
+                            <center><input type="submit" class="btn btn-primary m-t-10" name="submit" id="submitbtn" value="Lets go!" href="installation1.php"></center>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
